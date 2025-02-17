8d77b232e9661ee06466a46c676c724470c6484e
AU-HC <202004086@post.au.dk>
2025-02-17 20:37:32 +0100

    adds params to standard mayo

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	standard "mayo-go/mayo"
+)
 
 func main() {
-	fmt.Println("hello")
+	mayo := standard.InitMayo(16, 64, 66, 8, 9, 24, 32, 16)
+	pk, sk := mayo.CompactKeyGen()
+	fmt.Println(pk, sk)
 }
