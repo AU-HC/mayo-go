9b359534245dd9d0ffbb43724f2602920a593d63
AU-HC <202004086@post.au.dk>
2025-02-20 18:31:29 +0100

    adds ExpandSK function

--- mayo/mayo.go
+++ mayo/mayo.go
@@ -29,9 +29,8 @@ func (mayo *Mayo) CompactKeyGen() ([]byte, []byte, error) {
 
 	s := make([]byte, mayo.pkSeedBytes+mayo.oBytes)
 	h := sha3.NewSHAKE256()
-	_, _ = h.Write(seedSk[:])
+	_, _ = h.Write(seedSk[:mayo.pkSeedBytes])
 	_, _ = h.Read(s[:])
-
 	seedPk := s[:mayo.pkSeedBytes]
 	o := decodeMatrix(mayo.n-mayo.o, mayo.o, s[mayo.pkSeedBytes:mayo.pkSeedBytes+mayo.oBytes])
 
@@ -39,8 +38,8 @@ func (mayo *Mayo) CompactKeyGen() ([]byte, []byte, error) {
 	p := aes128ctr(seedPk, mayo.p1Bytes+mayo.p2Bytes)
 	p1 := decodeMatrixList(mayo.m, v, v, p[:mayo.p1Bytes], true)
 	p2 := decodeMatrixList(mayo.m, v, mayo.o, p[mayo.p1Bytes:mayo.p1Bytes+mayo.p2Bytes], false)
-	p3 := make([][][]byte, mayo.m)
 
+	p3 := make([][][]byte, mayo.m)
 	for i := 0; i < mayo.m; i++ {
 		p3[i] = multiplyMatrices(transposeMatrix(o), addMatrices(multiplyMatrices(p1[i], o), p2[i]))
 	}
@@ -51,6 +50,33 @@ func (mayo *Mayo) CompactKeyGen() ([]byte, []byte, error) {
 	csk := seedSk
 
 	return cpk, csk, nil
+}
+
+func (mayo *Mayo) ExpandSK(csk []byte) []byte {
+	// Parse csk
+	seedSk := csk[:mayo.skSeedBytes]
+
+	// Derive seedPk and O from seedSk
+	s := make([]byte, mayo.pkSeedBytes+mayo.oBytes)
+	h := sha3.NewSHAKE256()
+	_, _ = h.Write(seedSk[:mayo.pkSeedBytes])
+	_, _ = h.Read(s[:])
+	seedPk := s[:mayo.pkSeedBytes]
+	o := decodeMatrix(mayo.n-mayo.o, mayo.o, s[mayo.pkSeedBytes:mayo.pkSeedBytes+mayo.oBytes])
+
+	// Derive P1 and P2 from seedPk
+	v := mayo.n - mayo.o
+	p := aes128ctr(seedPk, mayo.p1Bytes+mayo.p2Bytes)
+	p1 := decodeMatrixList(mayo.m, v, v, p[:mayo.p1Bytes], true)
+	p2 := decodeMatrixList(mayo.m, v, mayo.o, p[mayo.p1Bytes:mayo.p1Bytes+mayo.p2Bytes], false)
+
+	// Compute the L
+	l := make([][][]byte, mayo.m)
+	for i := 0; i < mayo.m; i++ {
+		l[i] = addMatrices(multiplyMatrices(addMatrices(p1[i], transposeMatrix(p1[i])), o), p2[i])
+	}
+
+	return nil
 }
 
 func (mayo *Mayo) Sign() {
